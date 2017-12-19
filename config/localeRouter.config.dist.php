59b64e94e4f3ea50e4c5a53569fceb4463353b69
Maximilian Füsslin <maximilian.fuesslin@lotsearch.de>
2017-12-19 14:39:12 +0100

    updated composer and default config

---
+++ config/localeRouter.config.dist.php
@@ -0,0 +1,39 @@
+<?php
+
+$settings = [
+    // configure default language
+    //'defaultLocale' => 'de_DE',
+
+    // configure supported languages
+    //'languages' => ['de' => 'de_DE', 'en' => 'en_GB'],
+
+    // Adding extract strategies
+    /*'extractStrategies' => [
+        'extract-asset',
+        'extract-query',
+        [
+            'name'    => LocaleRouter\Strategy\Extract\UriPathStrategy::class,
+            'options' => [
+                'redirect_when_found' => true,
+            ],
+        ],
+        'extract-cookie',
+        'extract-acceptlanguage',
+    ],*/
+
+    // Adding persist strategies
+    /*'persistStrategies' => [
+        LocaleRouter\Strategy\Persist\DoctrineStrategy::class,
+        LocaleRouter\Strategy\Persist\CookieStrategy::class,
+    ],*/
+
+    // configure auth service (for Extract/UserIdentityStrategy or Persist/DoctrineStrategy)
+    //'authService' => 'zfcuser_auth_service',
+];
+
+/**
+ * Do not edit below this line!
+ */
+return [
+    'localeRouter' => $settings,
+];
