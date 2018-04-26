2567b6ca546fc4ef276bb63660aea1b3cd66fa40
Maximilian Füsslin <maximilian.fuesslin@lotsearch.de>
2018-04-26 10:24:16 +0200

    added more tests

--- tests/LocaleRouterTest/Factory/FactoryTest.php
+++ tests/LocaleRouterTest/Factory/FactoryTest.php
@@ -2,17 +2,25 @@
 
 namespace LocaleRouterTest\Factory;
 
+use Doctrine\ORM\EntityManager;
+use LocaleRouter\Entity\LocaleUserInterface;
 use LocaleRouter\Mvc\Router\Http\LanguageTreeRouteStack;
+use LocaleRouter\Strategy\Extract\UserIdentityStrategy;
+use LocaleRouter\Strategy\Persist\DoctrineStrategy;
+use LocaleRouter\Strategy\StrategyPluginManager;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
+use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\AuthenticationServiceInterface;
 use Zend\Router\Http\TreeRouteStack;
+use Zend\ServiceManager\ServiceManager;
 
 class FactoryTest extends TestCase
 {
     const LANGUAGE_EN = 'en_GB';
     const LANGUAGE_DE = 'de_DE';
 
-    /** @var ContainerInterface */
+    /** @var ServiceManager */
     protected $serviceManager;
 
     public function setUp()
@@ -45,5 +53,61 @@ class FactoryTest extends TestCase
 
         $treeRouteStack = $this->serviceManager->get(TreeRouteStack::class);
         $this->assertInstanceOf(LanguageTreeRouteStack::class, $treeRouteStack);
+    }
+
+    public function testUserIdentityStrategy()
+    {
+        $config = $this->serviceManager->get('Config');
+        $config['localeRouter']['authService'] = 'zfcuser_auth_service';
+
+        $this->serviceManager->setAllowOverride(true);
+        $this->serviceManager->setService('Config', $config);
+
+        $userMockValidLocale = $this->createMock(LocaleUserInterface::class);
+        $userMockValidLocale->expects($this->any())->method('getLocale')->will($this->returnValue('de_DE'));
+
+        $authServiceMock = $this->createMock(AuthenticationServiceInterface::class);
+        $authServiceMock->expects($this->any())
+            ->method('hasIdentity')
+            ->will($this->returnValue(true));
+
+        $authServiceMock->expects($this->any())
+            ->method('getIdentity')
+            ->will($this->returnValue($userMockValidLocale));
+
+        $this->serviceManager->setService('zfcuser_auth_service', $authServiceMock);
+        $this->serviceManager->setService(AuthenticationService::class, $authServiceMock);
+
+        $userIdentityStrategy =  $this->serviceManager->get(StrategyPluginManager::class)->get(UserIdentityStrategy::class);
+
+        $this->assertInstanceOf(UserIdentityStrategy::class, $userIdentityStrategy);
+    }
+
+    public function testDoctrineStrategy()
+    {
+        $config = $this->serviceManager->get('Config');
+        $config['localeRouter']['authService'] = 'zfcuser_auth_service';
+
+        $this->serviceManager->setAllowOverride(true);
+        $this->serviceManager->setService('Config', $config);
+
+        $userMockValidLocale = $this->createMock(LocaleUserInterface::class);
+        $userMockValidLocale->expects($this->any())->method('getLocale')->will($this->returnValue('de_DE'));
+
+        $authServiceMock = $this->createMock(AuthenticationServiceInterface::class);
+        $authServiceMock->expects($this->any())
+            ->method('hasIdentity')
+            ->will($this->returnValue(true));
+
+        $authServiceMock->expects($this->any())
+            ->method('getIdentity')
+            ->will($this->returnValue($userMockValidLocale));
+
+        $this->serviceManager->setService('zfcuser_auth_service', $authServiceMock);
+        $this->serviceManager->setService(AuthenticationService::class, $authServiceMock);
+
+        $doctrineStrategy =  $this->serviceManager->get(StrategyPluginManager::class)->get(DoctrineStrategy::class);
+
+        $this->assertInstanceOf(DoctrineStrategy::class, $doctrineStrategy);
     }
 }
