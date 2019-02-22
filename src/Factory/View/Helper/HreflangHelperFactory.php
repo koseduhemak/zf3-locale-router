bb40c8a23d8623b75c4cadda5bd87c0bcf88c45c
Maximilian Füsslin <mfuesslin@web.de>
2019-02-22 11:09:55 +0100

    updated with hreflang helper

---
+++ src/Factory/View/Helper/HreflangHelperFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace LocaleRouter\Factory\View\Helper;
+
+use Interop\Container\ContainerInterface;
+use LocaleRouter\Options\LanguageOptions;
+use LocaleRouter\View\Helper\HreflangHelper;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+class HreflangHelperFactory implements FactoryInterface
+{
+    public function __invoke(
+        ContainerInterface $container,
+        $requestedName,
+        array $options = null
+    ) {
+        /** @var LanguageOptions $options */
+        $options = $container->get(LanguageOptions::class);
+
+        if (!is_array($options->getLinks()) || empty($options->getLinks())) {
+            throw new \InvalidArgumentException('Please add a "localeRouter[links]" array configuration to your project in order for cron tasks to generate emails with absolute URIs');
+        }
+
+        return new HreflangHelper($options);
+    }
+}
